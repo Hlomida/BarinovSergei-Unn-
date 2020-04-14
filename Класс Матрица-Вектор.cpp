96b1a0daff9242ca23fd978a8cd72713bf0371da
Hlomida <58778076+Hlomida@users.noreply.github.com>
2020-04-14 14:52:39 +0300

    Update Класс Матрица-Вектор.cpp
    нерабочая проверка на правильность ввода

--- Класс Матрица-Вектор.cpp
+++ Класс Матрица-Вектор.cpp
@@ -1,23 +1,27 @@
 #include <iostream> 
 #include <cassert>
+#include <string>
 using namespace std;
+
 
 class Vector
 {
 public:
 	int x, y, z;
-	/*Vector(int x, int y, int z)
+	Vector()
+	{}
+	Vector(int x, int y, int z)
 	{
 		cout << "test 1" << endl;
 		cout << x << " " << y << " " << z << endl;
-	}*/
-	/*Vector(const Vector& vec)
+	}
+	Vector(const Vector& vec)
 	{
 			this->x = vec.x;
 			this->y = vec.y;
 			this->z = vec.z;
 		cout <<"Copy Iniciated" << endl;
-	}*/
+	}
 	Vector & operator = (const Vector& vec)
 	{
 
@@ -34,8 +38,10 @@ class Matrix
 {
 private:
 	int* a;
-	int collumn, row;
 public:
+	int collumn, row;
+	Matrix()
+	{}
 	Matrix(int _collumn, int _row) :
 		collumn(_collumn),
 		row(_row)
@@ -47,7 +53,7 @@ public:
 	{
 		this->collumn = mat.collumn;
 	this->row = mat.row;
-	
+
 		a = new int[collumn * row];
 		memcpy(a,mat.a,sizeof(collumn * row));
 		cout << "Copy Iniciated" << endl;
@@ -61,20 +67,36 @@ public:
 		return *this;
 	}
 };
+	
 
-int main()
-{
-	Vector a;
-	a.x = 1;
-	a.y = 5;
-	a.z = 3;
-	cout << a.x <<" "<<a.y<<" "<<a.z <<endl;
-	Vector b;
-	b.x = 3;
-	b.y = 7;
-	b.z = 4;
-	cout << b.x << " " << b.y << " " << b.z << endl;
-	a = b;
-	cout << a.x << " " << a.y << " " << a.z << endl;
-	return 0;
-};
+	int main()
+	{
+		int cx;
+		try
+		{
+			Vector c;
+			cin >> c.x;
+			if (!(isdigit(c.x)))
+			{
+				throw exception("Not a number imput");
+			}
+
+			cin >> c.y;
+			if (!(isdigit(c.y)))
+			{
+				throw exception("Not a number imput");
+			}
+			cin >> c.z;
+			if (!(isdigit(c.z)))
+			{
+				throw exception("Not a number imput");
+			}
+		cout << c.x << c.y << c.z << endl;
+		}
+		catch (const exception &ex)
+		{
+			cout << ex.what() << endl;
+		}
+		
+		return 0;
+	};
